3f6605d158d4d9a4e4f0d52712b3af415ffc2f1c
careerchangor <78281983+careerchangor@users.noreply.github.com>
2023-11-11 21:02:56 -0800

    Implemented user auth

--- back-end/index.js
+++ back-end/index.js
@@ -3,8 +3,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 app.use(express.json());
+app.use("/api/auth", authRoute);
 
 mongoose
   .connect(process.env.DB_URI, {
---
+++ back-end/routes/auth.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// 用户注册
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// 用户登录
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).json("User not found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) return res.status(400).json("Wrong password");
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
